3ad83ecc0e12961f656a501f8940ec12aa81a97b
VictorCiechovicz <victor.ciechovicz@merx.tech>
2024-05-23 21:23:40 -0300

    improve code

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,16 @@
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { HTTPException } from 'hono/http-exception'
 import { Hono } from 'hono'
 
-const app = new Hono().get('/', async c => {
+const app = new Hono().get('/', clerkMiddleware(), async c => {
+  const auth = getAuth(c)
+
+  if (!auth?.userId) {
+    throw new HTTPException(401, { res: c.json({ error: 'Unauhorized' }, 401) })
+  }
+
   const data = await db
     .select({
       id: accounts.id,
