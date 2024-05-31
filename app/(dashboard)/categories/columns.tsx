f2630c2d8da2f26f5f97e183b12fe990dcfaceef
VictorCiechovicz <victor.ciechovicz@merx.tech>
2024-05-31 20:05:05 -0300

    feat:create categories page

---
+++ app/(dashboard)/categories/columns.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { ColumnDef } from '@tanstack/react-table'
+import { ArrowUpDown, Edit, MoreHorizontal, Trash } from 'lucide-react'
+import { Checkbox } from '@/components/ui/checkbox'
+import { InferResponseType } from 'hono'
+import { client } from '@/lib/hono'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { useOpenAccount } from '@/hooks/useOpenAccount'
+import { ConfirmModal } from '@/components/ConfirmModal'
+import { useDeleteAccount } from '@/app/features/accounts/api/use-delete-account'
+
+export type ResponseType = InferResponseType<
+  typeof client.api.accounts.$get,
+  200
+>['data'][0]
+
+type ActionsProps = {
+  id: string
+}
+const Actions = ({ id }: ActionsProps) => {
+  const [openModalConfirm, setOpenModalConfirm] = useState(false)
+
+  const { onOpen } = useOpenAccount()
+  const deleteMutation = useDeleteAccount(id)
+
+  const handleDelete = async () => {
+    await deleteMutation.mutate()
+    setOpenModalConfirm(false)
+  }
+
+  return (
+    <>
+      <ConfirmModal
+        title="Are you sure?"
+        description="You are about perform a bulk delete"
+        openModalConfirm={openModalConfirm}
+        setOpenModalConfirm={setOpenModalConfirm}
+        handleSubmit={handleDelete}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            disabled={false}
+            onClick={() => setOpenModalConfirm(true)}
+          >
+            <Trash className="size-4 mr-2" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
+
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={value => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <Actions id={row.original.id} />
+  }
+]
---
+++ app/(dashboard)/categories/page.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useNewAccount } from '@/hooks/useNewAccounts'
+import { useGetAccounts } from '@/app/features/accounts/api/use-get-accounts'
+import { Loader2, Plus } from 'lucide-react'
+import { columns } from './columns'
+import { DataTable } from '@/components/DataTable'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useDeleteAccounts } from '@/app/features/accounts/api/use-delete-accounts'
+
+export default function CategoriesPage() {
+  const newAccount = useNewAccount()
+  const queryAccounts = useGetAccounts()
+  const deleteAccounts = useDeleteAccounts()
+
+  const isDisabled = queryAccounts.isLoading || deleteAccounts.isPending
+
+  if (isDisabled) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+            <Skeleton className="h-6 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+            Categories Page
+          </CardTitle>
+          <Button size="sm" onClick={newAccount.onOpen}>
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            filterKey="email"
+            columns={columns}
+            data={queryAccounts.data || []}
+            onDelete={row => {
+              const ids = row.map(r => r.original.id)
+              deleteAccounts.mutate({ ids })
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
