a3b6eaa50d35fae76823696615b19e59797a7a2f
VictorCiechovicz <victor09avila@hotmail.com>
2024-06-03 09:32:31 -0300

    feat:create trasactions schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-zod'
+import { z } from 'zod'
 
 export const accounts = pgTable('accounts', {
   id: text('id').primaryKey(),
@@ -8,6 +10,9 @@ export const accounts = pgTable('accounts', {
   userId: text('user_id').notNull()
 })
 
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions)
+}))
 export const insertAccountsSchema = createInsertSchema(accounts)
 
 export const categories = pgTable('categories', {
@@ -17,4 +22,40 @@ export const categories = pgTable('categories', {
   userId: text('user_id').notNull()
 })
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions)
+}))
 export const insertCategorySchema = createInsertSchema(categories)
+
+export const transactions = pgTable('transactions', {
+  id: text('id').primaryKey(),
+  amount: integer('amount').notNull(),
+  payee: text('payee').notNull(),
+  notes: text('notes'),
+  date: timestamp('date', { mode: 'date' }).notNull(),
+  accountId: text('account_id')
+    .references(() => accounts.id, {
+      onDelete: 'cascade'
+    })
+    .notNull(),
+  categoryId: text('category_id')
+    .references(() => categories.id, {
+      onDelete: 'set null'
+    })
+    .notNull()
+})
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  accounts: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id]
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id]
+  })
+}))
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date()
+})
