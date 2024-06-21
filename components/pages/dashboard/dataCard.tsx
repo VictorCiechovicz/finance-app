723a382b9aa716dc492474ac961d49da4788fef4
VictorCiechovicz <victor09avila@hotmail.com>
2024-06-21 20:57:55 -0300

    feat:create data card component

--- components/pages/dashboard/dataCard.tsx
+++ components/pages/dashboard/dataCard.tsx
@@ -1,1 +1,18 @@
-export const DataCard = () => {}
+type DataCardProps = {
+  title: string
+  value: any
+  percentageChange: any
+  icon: any
+  variant: any
+  dateRange: any
+}
+export const DataCard = ({
+  title,
+  value,
+  percentageChange,
+  icon,
+  variant,
+  dateRange
+}: DataCardProps) => {
+  return <div>datacard</div>
+}
