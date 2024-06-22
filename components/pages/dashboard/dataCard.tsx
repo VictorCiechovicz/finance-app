25f149f385875fc3ad74c37c0f04a83dea1358fe
VictorCiechovicz <victor09avila@hotmail.com>
2024-06-22 08:54:53 -0300

    improve code

--- components/pages/dashboard/dataCard.tsx
+++ components/pages/dashboard/dataCard.tsx
@@ -1,18 +1,72 @@
-type DataCardProps = {
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import { cva, VariantProps } from 'class-variance-authority'
+import { IconType } from 'react-icons/lib'
+
+const boxVariant = cva('rounded-md p-3', {
+  variants: {
+    variant: {
+      default: 'bg-blue-500/20',
+      success: 'bg-emerald-500/20',
+      danger: 'bg-rose-500/20',
+      warning: 'bg-yellow-500/20'
+    }
+  },
+  defaultVariants: {
+    variant: 'default'
+  }
+})
+
+const iconVariant = cva('size-6', {
+  variants: {
+    variant: {
+      default: 'fill-blue-500',
+      success: 'fill-emerald-500',
+      danger: 'fill-rose-500',
+      warning: 'fill-yellow-500'
+    }
+  },
+  defaultVariants: {
+    variant: 'default'
+  }
+})
+
+type BoxVariants = VariantProps<typeof boxVariant>
+type IconVariants = VariantProps<typeof iconVariant>
+
+interface DataCardProps extends BoxVariants, IconVariants {
   title: string
-  value: any
-  percentageChange: any
-  icon: any
-  variant: any
-  dateRange: any
+  value?: number
+  percentageChange?: number
+  icon: IconType
+  dateRange: string
 }
 export const DataCard = ({
   title,
-  value,
-  percentageChange,
-  icon,
+  value = 0,
+  percentageChange = 0,
+  icon: Icon,
   variant,
   dateRange
 }: DataCardProps) => {
-  return <div>datacard</div>
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+        <div className="space-y-2">
+          <CardTitle className="text-2xl line-clamp-1">{title}</CardTitle>
+          <CardDescription className="line-clamp-1">
+            {dateRange}
+          </CardDescription>
+        </div>
+        <div className={cn('shrink-0', boxVariant({ variant }))}>
+          <Icon className={cn(iconVariant({ variant }))} />
+        </div>
+      </CardHeader>
+    </Card>
+  )
 }
