f5bbe74d5d9d04cbf19f143039dea19ba21f5f09
VictorCiechovicz <victor09avila@hotmail.com>
2024-06-25 22:14:17 -0300

    feat: create pieVariant

--- components/pages/dashboard/dataCharts.tsx
+++ components/pages/dashboard/dataCharts.tsx
@@ -2,6 +2,7 @@
 
 import { useGetSummary } from '@/app/features/summary/api/use-get-summary'
 import { Chart } from './chart'
+import { SpendingPie } from './spendingPie'
 
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary()
@@ -13,6 +14,9 @@ export const DataCharts = () => {
     <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
       <div className="col-span-1 lg:col-span-3 xl:col-span-4">
         <Chart data={data?.days} />
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        <SpendingPie data={data?.categories} />
       </div>
     </div>
   )
--- components/pages/dashboard/spendingPie.tsx
+++ components/pages/dashboard/spendingPie.tsx
@@ -6,7 +6,10 @@ import {
   AreaChartIcon,
   BarChart2Icon,
   FileSearch,
-  LineChartIcon
+  LineChartIcon,
+  PieChart,
+  Radar,
+  Target
 } from 'lucide-react'
 import { AreaVariant } from './dataVariant'
 import { BarVariant } from './barVariant'
@@ -18,17 +21,17 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
+import { PieVariant } from './pieVariant'
 
 type Props = {
   data?: {
-    date: string
-    income: number
-    expenses: number
+    name: string
+    value: number
   }[]
 }
 
-export const Chart = ({ data = [] }: Props) => {
-  const [chartType, setChartType] = useState('area')
+export const SpendingPie = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState('pie')
 
   const onTypeChange = (type: string) => {
     setChartType(type)
@@ -36,28 +39,28 @@ export const Chart = ({ data = [] }: Props) => {
   return (
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
-        <CardTitle className="text-xl line-clamp-1">Transactions</CardTitle>
+        <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
         <Select defaultValue={chartType} onValueChange={onTypeChange}>
           <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
             <SelectValue placeholder="Chart type" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="area">
+            <SelectItem value="pie">
               <div className="flex items-center">
-                <AreaChartIcon className="size-4 mr-2 shrink-0" />
-                <p className="line-clamp-1">Area chart</p>
+                <PieChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Pie chart</p>
               </div>
             </SelectItem>
-            <SelectItem value="line">
+            <SelectItem value="radar">
               <div className="flex items-center">
-                <LineChartIcon className="size-4 mr-2 shrink-0" />
-                <p className="line-clamp-1">Line chart</p>
+                <Radar className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radar chart</p>
               </div>
             </SelectItem>
-            <SelectItem value="bar">
+            <SelectItem value="radial">
               <div className="flex items-center">
-                <BarChart2Icon className="size-4 mr-2 shrink-0" />
-                <p className="line-clamp-1">Bar chart</p>
+                <Target className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radial chart</p>
               </div>
             </SelectItem>
           </SelectContent>
@@ -72,9 +75,9 @@ export const Chart = ({ data = [] }: Props) => {
           </div>
         ) : (
           <>
-            {chartType === 'area' && <AreaVariant data={data} />}
-            {chartType === 'bar' && <BarVariant data={data} />}
-            {chartType === 'line' && <LineVariant data={data} />}
+            {chartType === 'pie' && <PieVariant data={data} />}
+            {chartType === 'radar' && <BarVariant data={data} />}
+            {chartType === 'radial' && <LineVariant data={data} />}
           </>
         )}
       </CardContent>
